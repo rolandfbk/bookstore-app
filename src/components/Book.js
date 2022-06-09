52cac42e15eefb3f2bc7942621340d6ad67ab01f
Roland <rolandfbk@gmail.com>
2022-06-09 17:48:35 +0200

    added missing elements

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,17 +1,34 @@
 import React from 'react';
 
 const SingleBook = (props) => {
-  const {author, title} = props.book[1][0];
+  const {author, title, category} = props.book[1][0];
   const id = props.book[0];
 
   return(
     <li className="book-li">
-      <div>
+      <div className="book-details">
+        <p className="category-name">{category}</p>
         <h2>{title}</h2>
-        <p>{author}</p>
+        <p className="book-author">{author}</p>
         <div className="book-button-actionList">
-          <button className="btn-delete-book" onClick={() => props.deleteBook(id)}>Delete</button>
+          <button className="btn-link-book">Comments</button>
+          <span className="book-panel">|</span>
+          <button className="btn-link-book" onClick={() => props.deleteBook(id)}>Remove</button>
+          <span className="book-panel">|</span>
+          <button className="btn-link-book">Edit</button>
         </div>
+      </div>
+      <div className="book-progress">
+        <div class="Oval-2"></div>
+        <div>
+          <p className="Percent-Complete">64%</p>
+          <p className="Completed">Completed</p>
+        </div>
+      </div>
+      <div className="book-chapter">
+        <p className="Current-Chapter">CURRENT CHAPTER</p>
+        <p className="Current-Lesson">Chapter 17</p>
+        <button className="Update-progress">Update progress</button>
       </div>
     </li>
   );
